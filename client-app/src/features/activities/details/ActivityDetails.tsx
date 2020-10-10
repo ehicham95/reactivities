13ac7352762d77a3cd26090edeacf7ebd21da946
El <e.hicham95@gmail.com>
2020-10-10 15:57:39 +0200

    Add validators using revalidate & react-widgets

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,48 +1,45 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
-import { ActivityDetailedHeader } from './ActivityDetailedHeader';
-import { ActivityDetailedInfo } from './ActivityDetailedInfo';
-import { ActivityDetailedChat } from './ActivityDetailedChat';
-import { ActivityDetailedSideBar } from './ActivityDetailedSideBar';
+import ActivityDetailedHeader from './ActivityDetailedHeader';
+import ActivityDetailedInfo from './ActivityDetailedInfo';
+import ActivityDetailedChat from './ActivityDetailedChat';
+import ActivityDetailedSidebar from './ActivityDetailedSidebar';
 
 interface DetailParams {
-    id: string;
+  id: string;
 }
 
-const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({match, history}) => {
-    const activityStore = useContext(ActivityStore);
-    const {activity, 
-        loadActivity, 
-        loadingInitial
-    } = activityStore;
-    
-    useEffect(() => {
-        loadActivity(match.params.id);
-    }, [loadActivity, match.params.id, history]);
+const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history
+}) => {
+  const activityStore = useContext(ActivityStore);
+  const { activity, loadActivity, loadingInitial } = activityStore;
 
-    if(loadingInitial) return <LoadingComponent content='Loading activity ...'/>
+  useEffect(() => {
+    loadActivity(match.params.id);
+  }, [loadActivity, match.params.id, history]);
 
-    if(!activity)
-    {
-        return <h2>Activity Not Found</h2>
-    }
-    
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <ActivityDetailedHeader activity={activity}/>
-                <ActivityDetailedInfo activity={activity}/>
-                <ActivityDetailedChat activity={activity}/>
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <ActivityDetailedSideBar/>
-            </Grid.Column>
-        </Grid>
-    )
-}
+  if (loadingInitial) return <LoadingComponent content='Loading activity...' />;
+
+  if (!activity) return <h2>Activity not found</h2>;
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetailedHeader activity={activity} />
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetailedSidebar />
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default observer(ActivityDetails);
